02403357f926c8e3d2dd804529965a61f64df7c1
pancakesontuesday <mulberry3280@korea.ac.kr>
2023-07-08 19:45:16 +0900

    chechpoint~

--- Madcamp-Week2/Assets/Scripts/Player.cs
+++ Madcamp-Week2/Assets/Scripts/Player.cs
@@ -4,43 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    GameManager gameManager;
     public Animator anim;
-    public Stairs stairs;
-    public int playerLoc, score;
-    public bool isDead;
-    public bool facingRight;
+    
+    public bool isDead = false, facingRight = false;
+    public int playerIdx = 0, playerLoc = 0;
     
     void Awake() {
         anim = gameObject.GetComponent<Animator>();
-        stairs = GameObject.FindObjectOfType<Stairs>();
-    }
-
-    void Start() {
-        playerLoc = 0;
-        score = 0;
-        isDead = false;
-        facingRight = false;
-    }
-
-    public void Step(bool isStepRight) {
-
-        // anim.SetBool("facingRight", isStepRight); // change sprite accordingly
-
-        // check if correct
-        if (stairs.isRight[playerLoc] == isStepRight) {
-            // correct
-            Debug.Log("Correct");
-            score++;
-
-            stairs.StairMove(isStepRight);
-            playerLoc = (playerLoc + 1) % 20;
-        } else {
-            // wrong
-            Debug.Log("Wrong");
-            isDead = true;
-        } 
-
-        // reset timer
     }
 
 }
