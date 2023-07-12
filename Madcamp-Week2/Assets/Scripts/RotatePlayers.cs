46c68877e76161bfc1cc964286c5e7c547e04d49
pancakesontuesday <mulberry3280@korea.ac.kr>
2023-07-12 16:41:30 +0900

    FINAL

--- Madcamp-Week2/Assets/Scripts/RotatePlayers.cs
+++ Madcamp-Week2/Assets/Scripts/RotatePlayers.cs
@@ -10,18 +10,4 @@ public class RotatePlayers : MonoBehaviour
         // Rotate the center GameObject
         transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
-
-    public void ChangeCharacter(int newIndex) {
-        Debug.Log("ChangeCharacter: " + newIndex);
-
-        gm.players[gm.playerIdx].SetActive(false);
-        gm.playerParent.SetActive(false);
-        
-        gm.players[newIndex].SetActive(true);
-        gm.player = gm.players[newIndex].GetComponent<Player>();
-        gm.playerParent = gm.player.transform.parent.gameObject;
-        gm.playerParent.SetActive(true);
-
-        gm.playerIdx = newIndex;
-    }
 }
